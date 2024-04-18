a81dd034e24f2d156f9fd885ad5e10a894437553
Michael A. Hawker <hawkerm@users.noreply.github.com>
2024-04-18 01:59:57 -0700

    Start trying to setup BingoCard component with a similar Font
    Keep on Truckin' fron FontZillion

---
+++ src/components/BingoCard.vue
@@ -0,0 +1,54 @@
+<script setup lang="ts">
+/*defineProps<{
+  msg: string
+}>()*/
+</script>
+
+<template>
+  <div class="card">
+    <div class="letters">
+        <div><span>S</span></div>
+        <div><span>A</span></div>
+        <div><span>M</span></div>
+        <div><span>G</span></div>
+        <div><span>O</span></div>
+    </div>
+    <!-- Bingo Tile -->
+  </div>
+</template>
+
+<style scoped>
+@font-face {
+  font-family: "Keep on Truckin";
+  src: local("Keep on Truckin"), url(@/assets/fonts/keept___.ttf) format("truetype");
+}
+
+.card {
+  background-color: #f76564;
+  border-radius: 24px;
+
+  height: 92px;
+  padding: 0x 12px 0px 12px;
+}
+
+.letters {
+  font-family: "Keep on Truckin", 'Times New Roman', Times, serif;
+  font-weight: bold;
+  font-size: 64px;
+
+  color: white;
+
+  > div {
+    float: left;
+    margin: 0 12px 0 12px;
+    text-align: center;
+  }
+}
+
+/*@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}*/
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-import TheWelcome from '../components/TheWelcome.vue'
+import BingoCard from '../components/BingoCard.vue'
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <BingoCard />
   </main>
 </template>

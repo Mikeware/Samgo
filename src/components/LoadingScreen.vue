43393b44d5574e5083ce7f575a98e12c5fda707c
Michael A. Hawker <hawkerm@users.noreply.github.com>
2024-04-19 20:23:05 -0700

    Setup initial staging of Loading Screen and Game Changer Bars

---
+++ src/components/LoadingScreen.vue
@@ -0,0 +1,73 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+/*defineProps<{
+  msg: string
+}>()*/
+</script>
+
+<template>
+  <div class="bands">
+    <div class="bar first"></div>
+    <div class="bar second"></div>
+    <div class="bar third"></div>
+    <div class="bar fourth"></div>
+    <div class="bar fifth"></div>
+  </div>
+</template>
+
+<style scoped>
+.bands {
+  display: flex;
+  flex-direction: column;
+  width: 100vw;
+  height: 100vh;
+}
+
+.bar {
+  flex: 1;
+
+  animation: slideIn 2s forwards;
+}
+
+.bar.second, .bar.fourth {
+  animation: slideInFromLeft 2s forwards;
+}
+
+@keyframes slideIn {
+  0% {
+    transform: translateX(100%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+
+@keyframes slideInFromLeft {
+  0% {
+    transform: translateX(-100%);
+  }
+  100% {
+    transform: translateX(0);
+  }
+}
+
+.first {
+  background-color: #b52327;
+}
+
+.second {
+  background-color: #ef6638;
+}
+
+.third {
+  background-color: #fdbe4f;
+}
+
+.fourth {
+  background-color: #2088a7;
+}
+
+.fifth {
+  background-color: #711931;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,20 @@
 <script setup lang="ts">
-import BingoCard from '../components/BingoCard.vue'
+import BingoCard from '@/components/BingoCard.vue'
+import LoadingScreen from '@/components/LoadingScreen.vue';
+import { ref, onMounted } from 'vue'
+
+const isLoading = ref(true);
+
+onMounted(() => {
+  setTimeout(() => {
+    ///isLoading.value = false;
+  }, 2000);
+})
 </script>
 
 <template>
   <main>
-    <BingoCard />
+    <LoadingScreen v-if="isLoading" />
+    <BingoCard v-if="!isLoading" />
   </main>
 </template>

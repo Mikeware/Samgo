d00e8a7881eb8b4f8cd198f07f0ae958ddf25efc
Michael A. Hawker <8959496+hawkerm@users.noreply.github.com>
2025-08-29 13:24:41 -0700

    Rest of clean-up of display of the ShareDialog

--- src/components/ShareDialog.vue
+++ src/components/ShareDialog.vue
@@ -8,7 +8,7 @@ const props = defineProps<{
 const shareCode = ref()
 
 onMounted(() => {
-  shareCode.value = window.location.origin + '?board='
+  shareCode.value = window.location.origin + '/?board='
 
   // Generate board code by adding 2-digit HEX value to represent each phrase index
   for (let i = 0; i < 25; i++) {
@@ -47,15 +47,23 @@ function copyCodeToClipboard() {
       </svg>
     </button>
     <p>Share this link with friends to show off your winning board!</p>
-    <input type="text" :value="shareCode" readonly />
-    <button @click="copyCodeToClipboard()" type="button" class="flat-button">
-      <!--!Font Awesome Free v7.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="16" height="16">
-        <path
-          d="M192 0c-35.3 0-64 28.7-64 64l0 256c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-200.6c0-17.4-7.1-34.1-19.7-46.2L370.6 17.8C358.7 6.4 342.8 0 326.3 0L192 0zM64 128c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-16-64 0 0 16-192 0 0-256 16 0 0-64-16 0z"
-        />
-      </svg>
-    </button>
+    <div class="share-link-row">
+      <input
+        type="text"
+        :value="shareCode"
+        @click="copyCodeToClipboard()"
+        readonly
+        class="share-link-input"
+      />
+      <button @click="copyCodeToClipboard()" type="button" class="flat-button">
+        <!--!Font Awesome Free v7.0.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2025 Fonticons, Inc.-->
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" width="16" height="16">
+          <path
+            d="M192 0c-35.3 0-64 28.7-64 64l0 256c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-200.6c0-17.4-7.1-34.1-19.7-46.2L370.6 17.8C358.7 6.4 342.8 0 326.3 0L192 0zM64 128c-35.3 0-64 28.7-64 64L0 448c0 35.3 28.7 64 64 64l192 0c35.3 0 64-28.7 64-64l0-16-64 0 0 16-192 0 0-256 16 0 0-64-16 0z"
+          />
+        </svg>
+      </button>
+    </div>
   </div>
 </template>
 
@@ -80,6 +88,27 @@ function copyCodeToClipboard() {
     0 0 0 18px #ef6638,
     0 0 0 24px #fdbe4f,
     0 0 0 30px #b52327;
+}
+
+/* Flex row for input and button */
+.share-link-row {
+  display: flex;
+  align-items: center;
+  gap: 0.75em;
+  margin-top: 1em;
+  margin-bottom: 1em;
+}
+
+.share-link-input {
+  flex: 1;
+  border: none;
+  background: #f7f7f7;
+  padding: 0.5em 0.75em;
+  border-radius: 8px;
+  font-size: 1em;
+  color: #333;
+  outline: none;
+  min-width: 0;
 }
 
 /* https://getcssscan.com/css-buttons-examples */
@@ -109,7 +138,7 @@ function copyCodeToClipboard() {
 /* Align to top-right of div */
 .close-button {
   position: absolute;
-  top: 1em;
-  right: 1em;
+  top: 1.5em;
+  right: 1.5em;
 }
 </style>
